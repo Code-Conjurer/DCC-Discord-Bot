c89a24dbfffba7f75216491f7d88b9707b68cd3c
Matthew Garneau <sadzune@gmail.com>
2021-02-22 15:10:10 -0800

    table command now allows for roll to be given. tableData now allows for rolls under 1

--- commands/tableCmd.ts
+++ commands/tableCmd.ts
@@ -3,10 +3,14 @@ import Discord = require('discord.js');
 import BoxDrawing from "../utilities/BoxDrawing";
 
 export function rollOnTable (message: Discord.Message, args: string[]){
-    const tableName = args.join('_');
-
-    const range = getFromTable.tableRange(tableName);
-    const roll = Math.floor(Math.random() * range) + 1;
+    const tableName = args[0];
+    let roll: number;
+    if(args.length > 1){
+        roll = parseInt(args[1]);
+    }else{
+        const range = getFromTable.tableRange(tableName);
+        roll = Math.floor(Math.random() * range) + 1;
+    }
 
     let resultData = `roll: ${roll}\n`
     const tableResult: {[key: string]: any} = getFromTable.tableData(tableName, roll);
--- utilities/getFromTable.ts
+++ utilities/getFromTable.ts
@@ -1,39 +1,38 @@
 import fs = require('fs');
 
 const tableFiles: string[] = fs.readdirSync('./tables').filter(file => file.endsWith('.json')); // we need to step out of dist and access the main tables
-let tables: {[name:string]: {roll: number, table: {[key: string]: object}}} = {};
+let tables: { [name: string]: { roll: number, table: { [key: string]: object } } } = {};
 
 tableFiles.forEach(tableFile => {
-    if(tableFile.includes(' ')) throw new Error("table name contains space");
-    
+    if (tableFile.includes(' ')) throw new Error("table name contains space");
+
     const table = require(`../../tables/${tableFile}`);
-    const name = tableFile.replace('.json','');
+    const name = tableFile.replace('.json', '');
     tables[name] = table;
 });
 
-function tableData(tableName: string, roll?: number): {[key: string]: any}{
-    if(tables[tableName] == undefined) return {};
+function tableData(tableName: string, roll: number): { [key: string]: any } {
+    if (tables[tableName] == undefined) return {};
 
-    const range = tableRange(tableName);
 
-    if(roll === undefined) roll = Math.floor(Math.random() * range) + 1;
-    else if(roll > range || roll < 1) return {};
 
     const table = tables[tableName];
 
-    while(roll > 0 && table.table[String(roll)] == undefined){
-        roll--;
+    while (roll > 0 && table.table[String(roll)] == undefined) {
+        if (roll > 0)
+            roll--;
+        else
+            roll++;
     }
-
-    if(roll == 0) throw new Error(`table: ${tableName} has no lower bound`);
+    if (roll == 0) throw new Error(`table: ${tableName} has no lower bound`);
 
     return table.table[String(roll)];
 }
 
-function tableRange(tableName: string): number{
-    if(tables[tableName] == undefined) return -1;
+function tableRange(tableName: string): number {
+    if (tables[tableName] == undefined) return -1;
 
     return tables[tableName].roll;
 }
 
-export default {tableData, tableRange};
+export default { tableData, tableRange };
